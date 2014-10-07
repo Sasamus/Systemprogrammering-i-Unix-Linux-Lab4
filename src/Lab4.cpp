f182b4cb19afeda9870c674f740970015d7c8377
Sasamus <labus93@gmail.com>
2014-10-07 15:00:25 +0200

    First commit

---
+++ src/Lab4.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : Lab4.cpp
+// Author      : Albin Engström
+// Created     : 2014-10-07
+// Modified    : 2014-10-07
+// Description : main for project Lab4
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+
+	return 0;
+}
