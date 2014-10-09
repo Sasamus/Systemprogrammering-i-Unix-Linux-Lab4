0bf454092c302a39124a305f4b5a8399b8ce5ca0
Sasamus <labus93@gmail.com>
2014-10-09 15:14:22 +0200

    Fifth commit

--- src/Lab4.cpp
+++ src/Lab4.cpp
@@ -1,22 +1,22 @@
 //=============================================================
-// Name        : Lab4.cpp
-// Author      : Albin Engström
-// Created     : 2014-10-07
-// Modified    : 2014-10-07
-// Description : main for project Lab4
-// Purpose	   : Start the program
+// Name         : Lab4.cpp
+// Author       : Albin Engström
+// Created      : 2014-10-07
+// Modified     : 2014-10-07
+// Description  : main for project Lab4
+// Purpose      : Start the program
 //=============================================================
 #include "ProcessManager.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
+    //Create a ProcessManager object
+    ProcessManager o_ProcessManager;
 
-	//Create a ProcessManager object
-	ProcessManager o_ProcessManager;
-
-	//Call it's Run function
-	o_ProcessManager.Run();
-
-	return 0;
+    //Call it's Run function
+    o_ProcessManager.Run();
+    
+    return 0;
 }
